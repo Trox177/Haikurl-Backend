d222f5668d94b7012f3f8c745b120fa9458e101c
Daniel Haag <dhaag@fordham.edu>
2022-04-02 15:57:47 -0400

    Upgrade Regex

--- backend/validations/urlValidations.js
+++ backend/validations/urlValidations.js
@@ -8,7 +8,7 @@ const Joi = require('joi');
  * Joi validater that checks if the request body is a json objecting containing a url string
  */
 const urlSchema = Joi.object().keys({
-  url: Joi.string().domain({ tlds: false }),
+  url: Joi.string().pattern(new RegExp("((http|https)://)(www.)?[a-zA-Z0-9@:%._\\+~#?&//=]{2,256}\\.[a-z]{2,6}\\b([-a-zA-Z0-9@:%._\\+~#?&//=]*)"))
 });
 
 module.exports = { urlSchema };
